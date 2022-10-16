af2767a78fb5a69b469b7b099fa2c32432364a85
KiilaAllu <46685014+KiilaAllu@users.noreply.github.com>
2022-10-16 19:51:18 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,29 @@
+const chalk = require('chalk')
+const yargs = require('yargs')
+const getNotes = require('./notes.js')
+
+// Customize yargs version
+yargs.version ('1.1.0')
+
+// Create add command
+yargs.command({
+    command: 'add',
+    describe: 'Add a new note',
+    handler: function () {
+        console.log('Adding a new note!')
+    }
+})
+
+// Create remove command
+yargs.command({
+    command: 'remove',
+    describe: 'Remove a note',
+    hander: function () {
+        console.log('Removing the note')
+    }
+})
+
+// add, remove, read, list
+
+
+console.log(yargs.argv)
